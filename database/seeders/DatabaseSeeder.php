1bc9c9daac7a899e9a3c3f31e5ddde6df48f1551
0HMaxim <83365278+0HMaxim@users.noreply.github.com>
2026-06-15 01:28:17 +0200

    Add CategoriesSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesAndPermissionsSeeder::class,
             CurrenciesSeeder::class,
+            CategoriesSeeder::class,
         ]);
     }
 }
